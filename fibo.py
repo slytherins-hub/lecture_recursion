3b6b9319f2dae26955c9e323a8e854c25417eef1
Libuse Hopjanova <247243@vutbr.cz>
2023-04-24 09:45:59 +0200

    h

--- fibo.py
+++ fibo.py
@@ -1,6 +1,21 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
+
 
 def main():
-    pass
+    n = int(input("počet členů Fibonacciho posloupnosti:"))
+    seq = []
+    for num in range(n):
+        seq.append(recursive_nth_fibo(num))
+    print(seq)
+
 
 
 if __name__ == "__main__":
