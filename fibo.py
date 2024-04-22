088ca80b620f3416ead4aad5f4a16c266005cff3
Filip Sulik <257054@vutbr.cz>
2024-04-22 12:27:11 +0200

    created function recursive_nth_fibo()

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    """
+    Returns n-th value of Fibonaci sequence
+    :param n: (int) position of value
+    :return: (int) value at n-th position
+    """
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(6))
 
 
 if __name__ == "__main__":
