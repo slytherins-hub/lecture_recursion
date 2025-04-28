a5fd32e06d88297d75bd634b951bfd7720ca00e7
TerezaStenclova <terezka.stenclova@gmail.com>
2025-04-28 09:37:48 +0200

    done

--- fibo.py
+++ fibo.py
@@ -1,7 +1,23 @@
+def recursive_nth_fibo(n):
+
+    if n <=0: #podminka ukonceni
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
+    n = int(input("Zadejte počet prvků Fibonacciho posloupnosti: "))
+    seznam = list()
+    for i in range(n):
+        seznam.append(recursive_nth_fibo(i))
+    print(seznam)
+    print(seznam[-1])
+    # poro in=4 [0, 1, 1, 2] #1,1 pro jeden par kraliku mam 1 moznost
 
 
 if __name__ == "__main__":
     main()
+
+
